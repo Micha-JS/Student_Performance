6b4b1de215c1b7c2b0519f1c36ae027ca27703cc
Micha <111045381+Micha-JS@users.noreply.github.com>
2023-05-01 14:11:03 +0200

    Create folder structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='We are paticipating at the Predict Student Performance from Game Play competition on Kaggle. In the repo we collect our ideas to achieve the best possible prediction.',
+    author='Elizaveta, Mounika, Michael',
+    license='MIT',
+)
